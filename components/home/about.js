3128222d481a9727a572b8b7ddafc6ab30cddde5
Gulam Anas <g.anas786@gmail.com>
2023-04-07 15:41:16 +0530

    about part for main page done

--- components/home/about.js
+++ components/home/about.js
@@ -3,13 +3,13 @@ import React from 'react';
 const About = () => {
   return (
     <>
-      <h3>About Me</h3>
-      <div>
+      <h3 className='text-5xl font-semibold'>About Me</h3>
+      <p className='text-2xl font-medium mt-7'>
         Hello, I&apos;m Gulam Anas, a frontend developer working for Mobignosis
         Pvt. Ltd. in Bengaluru, India. I&apos;m enthusiastic about expanding my
         technological skills and am presently engrossed in learning and applying
         React.
-      </div>
+      </p>
     </>
   );
 };
