462f96086fd7362d58383ad436c44121cd56163b
Gulam Anas <g.anas786@gmail.com>
2023-04-08 11:08:34 +0530

    added more section and styled properly

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Image from 'next/image';
+import logo from '../assets/ga.png';
 
 const Navigation = () => {
   return (
-    <nav className='flex justify-around items-center h-16 bg-red-300'>
-      <div className='brand'>Portfolio</div>
+    <nav className='flex justify-around items-center h-16 bg-white shadow-lg sticky top-0'>
+      <div className='w-10 overflow-hidden '>
+        <Image src={logo} alt='Logo' width='auto' height='auto' />
+      </div>
       <ul className='flex justify-between gap-3'>
         <li>
           <a href='#'>Home</a>
---
+++ components/home/header.js
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import profile from '../../assets/anas-dp.png';
+
+const Header = () => {
+  return (
+    <header className='flex items-center justify-around h-screen'>
+      <div className='text-5xl font-semibold leading-snug'>
+        <span className=' text-[#0087CA]'>Hello!</span>
+        <br />
+        My Name is <span className='text-[#0087CA]'>Anas</span>
+        <br />
+        and I&apos;m a <br />
+        <span className='text-[#0087CA]'>FrontEnd Developer.</span>
+      </div>
+      <div className='images rounded-full overflow-hidden'>
+        <Image src={profile} alt='Profile pic' width='auto' height='auto' />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
