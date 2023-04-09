81f328a2a30d4fb967cee326707d4efd64ccb0d4
Gulam Anas <g.anas786@gmail.com>
2023-04-09 22:59:15 +0530

    navbar

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import logo from '../assets/ga.png';
 
 const Navigation = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleResponsive = () => {
+    setNav(!nav);
+  };
+
   return (
     <nav className='flex justify-around items-center h-16 bg-white shadow-lg sticky top-0'>
       <div className='w-10 overflow-hidden '>
         <Image src={logo} alt='Logo' width='auto' height='auto' />
       </div>
-      <ul className='flex justify-between gap-3'>
-        <li>
-          <a href='#'>Home</a>
-        </li>
-        <li>
-          <a href='#'>About</a>
-        </li>
-        <li>
-          <a href='#'>Project</a>
-        </li>
-        <li>
-          <a href='#'>Contact</a>
-        </li>
+      <div
+        className='navbar sm:hidden cursor-pointer'
+        onClick={handleResponsive}
+      >
+        <div className='div w-6 h-1 bg-red-500 m-1'></div>
+        <div className='div w-6 h-1 bg-red-500 m-1'></div>
+        <div className='div w-6 h-1 bg-red-500 m-1'></div>
+      </div>
+      <ul
+        id='listItems'
+        className={`fixed top-16 left-0 w-full shadow-lg sm:shadow-none ${
+          nav ? 'scale-100' : 'scale-y-0 '
+        } transition-all origin-top flex flex-col gap-3 bg-white sm:flex-row sm:static sm:justify-between sm:w-auto`}
+      >
+        {navLinks.map(data => {
+          return (
+            <li key={data.id}>
+              <a href='#'>{data.name}</a>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
@@ -28,7 +42,9 @@ const Navigation = () => {
 
 const navLinks = [
   { id: 1, name: 'Home', link: '/' },
-  { id: 2, name: 'About', link: '/' },
+  { id: 2, name: 'About', link: '/about' },
+  { id: 3, name: 'Project', link: '/project' },
+  { id: 4, name: 'Contact Us', link: '/contact' },
 ];
 
 export default Navigation;
